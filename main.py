012ac541f78fd8261c11e651d4ded1788338cf30
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-07-31 13:07:34 -0400

    adds player file

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ SEED = random.randint(0, 1000000000000000000000000000)
 window.fullscreen = True
 window.exit_button.visible = False
 window.vsync = True
-window.title = 'Voxel Based Block Game'
 
 terrain = Terrain(terrainSize=SIZE, terrainHeight=HEIGHT, chunkThreads=CHUNK_THREADS, seed=SEED)
 
 xChunk = len(terrain.chunks)//2
 yChunk = len(terrain.chunks[xChunk])//2
-xBlock = len(terrain.chunks[xChunk][yChunk].blocks)//2
-yBlock = len(terrain.chunks[xChunk][yChunk].blocks[xBlock])//2
 
-player.startPos.y = len(terrain.chunks[xChunk][yChunk].blocks[xBlock][yBlock])
+player.startPos.y = len(terrain.chunks[xChunk][yChunk].blocks[8][8])
 player.position = player.startPos
 
 if __name__ == '__main__':
